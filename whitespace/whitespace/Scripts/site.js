5bb2de89dcce0457d2a69ac19bb3ec5c60710f2c
HARRIS\h13638 <dbachowski@harrislocalgov.com>
2015-03-21 23:47:14 -0400

    First commmit

---
+++ whitespace/whitespace/Scripts/site.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    var resizeSections = function () {
+        var height = $(window).height() - 50;
+        $(".section").height(height);
+    }
+
+    resizeSections();
+
+    $(window).resize(function () {
+        resizeSections();
+    });
+});
+
