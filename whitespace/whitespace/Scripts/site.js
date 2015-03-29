e54f7efef3b42470037a1eeedc0f1ffd1a94db8b
HARRIS\h13638 <dbachowski@harrislocalgov.com>
2015-03-29 18:08:00 -0400

    Putting fixed background classes in;
    Adding Team background;

--- whitespace/whitespace/Scripts/site.js
+++ whitespace/whitespace/Scripts/site.js
@@ -1,7 +1,20 @@
 ﻿$(document).ready(function () {
     var resizeSections = function () {
-        var height = $(window).height() - 50;
-        $(".section").height(height);
+        var windowHeight = $(window).height() - 50;
+
+        $('.section').each(function (i, obj) {
+            var sectionHeight = windowHeight;
+            if ($(obj).height() > sectionHeight) {
+                sectionHeight = $(obj).height();
+            }
+
+            $(obj).height(sectionHeight);            
+            //obj.css('min-height', sectionHeight);
+        });
+
+        //var sectionHeight = Math.max(windowHeight, $(".section").height());
+        //$(".section").height(sectionHeight);
+        //$('.section').css('min-height', windowHeight);
     }
 
     resizeSections();
